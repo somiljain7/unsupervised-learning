ed5bc619ce5209e03d68d84fd2a57fe1a564fcb4
SOMIL JAIN <42209500+somiljain7@users.noreply.github.com>
2020-05-19 15:24:54 +0530

    inertia demo kmeans #2

---
+++ inertia.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans 
+ks = range(1, 6)
+inertias = []
+
+for k in ks:
+    # Create a KMeans instance with k clusters: model
+    model = KMeans(n_clusters=k)    
+    # Fit model to samples
+    model.fit(samples)
+    #data source nhi mentioned hai 
+    # Append the inertia to the list of inertias
+    inertias.append(model.inertia_)
+    
+# Plot ks vs inertias
+plt.plot(ks, inertias, '-o')
+plt.xlabel('number of clusters, k')
+plt.ylabel('inertia')
+plt.xticks(ks)
+plt.show()
