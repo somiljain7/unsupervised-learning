6330639a12b135507ff9998c86e86c73d8e43f3d
SOMIL JAIN <42209500+somiljain7@users.noreply.github.com>
2020-05-20 18:51:38 +0530

    #2

--- wiki.py
+++ wiki.py
@@ -11,3 +11,16 @@ kmeans = KMeans(n_clusters=6)
 
 # Create a pipeline: pipeline
 pipeline = make_pipeline(svd, kmeans)
+# Import pandas
+import pandas as pd
+
+# Fit the pipeline to articles
+pipeline.fit(articles)
+
+# Calculate the cluster labels: labels
+labels = pipeline.predict(articles)
+# Create a DataFrame aligning labels and titles: df
+df = pd.DataFrame({'label': labels, 'article': titles})
+
+# Display df sorted by cluster label
+print(df.sort_values('label'))
